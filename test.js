96868e3d095e713b50819683de995b7be4379299
ls-5s <2408224899@qq.com>
2025-10-30 09:45:15 +0800

    feat:sort 背后原理是什么

--- test.js
+++ test.js
@@ -1,11 +1,9 @@
-var arr1 = [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10]
-function unique(arr) {
- let res = []
- for(let i= 0;i <arr.length;i++) {
-    if(res.indexOf(arr[i]) === -1) {
-      res.push(arr[i])
-    }
- }
-  return res
-}
-console.log(unique(arr1))
+const userArr = [
+    { name: '张三', age: 25 },
+    { name: '李四', age: 20 },
+    { name: '王五', age: 30 }
+];
+// 按 age 升序排序
+userArr.sort((a, b) => b.age - a.age);
+console.log(userArr);
+// 输出：[{name: '王五', age: 30}, {name: '张三', age: 25}, {name: '李四', age: 20}]
