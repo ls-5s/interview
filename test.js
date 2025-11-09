2220492e9130846d4742c9e567e8c73b20b2549d
ls-5s <2408224899@qq.com>
2025-11-09 17:16:13 +0800

    文件初始化

--- test.js
+++
@@ -1,9 +0,0 @@
-const userArr = [
-    { name: '张三', age: 25 },
-    { name: '李四', age: 20 },
-    { name: '王五', age: 30 }
-];
-// 按 age 升序排序
-userArr.sort((a, b) => b.age - a.age);
-console.log(userArr);
-// 输出：[{name: '王五', age: 30}, {name: '张三', age: 25}, {name: '李四', age: 20}]
